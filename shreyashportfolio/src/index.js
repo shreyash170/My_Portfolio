9b6e97ae2acdb69311002710d998888f6556d7c5
shreyash170 <shreyashgupta.170@gmail.com>
2024-03-20 23:45:41 +0530

    Deploy changes

--- shreyashportfolio/src/index.js
+++ shreyashportfolio/src/index.js
@@ -9,7 +9,7 @@ import "@fontsource/roboto";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename="/">
+  <HashRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
