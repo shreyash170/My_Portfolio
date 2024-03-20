c6b561c2c1003fe63906a682f762290ddf7c1d5a
shreyash170 <shreyashgupta.170@gmail.com>
2024-03-20 21:16:35 +0530

    Deploy changes

--- shreyashportfolio/src/App.js
+++ shreyashportfolio/src/App.js
@@ -5,13 +5,13 @@ import Skills from "./Component/Skills";
 import Projects from "./Component/Projects";
 import Contact from "./Component/Contact";
 import Footer from "./Component/Footer";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import "./Styles/MediaQuery.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/My_Portfolio">
+      <HashRouter>
         <Home />
         <Section />
         <About />
@@ -19,7 +19,7 @@ function App() {
         <Projects />
         <Contact />
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
